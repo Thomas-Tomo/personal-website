9945e371e8638ab49cbd29917e9be9a2c15fe97a
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-06-03 18:57:31 +0100

    Refactor NavBar to use React bootstrap

--- my-website/src/components/Header.js
+++ my-website/src/components/Header.js
@@ -1,30 +1,22 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import styles from "../assets/styles/Header.module.css";
 
 function Header() {
   return (
-    <Navbar expand="md" className={styles.header}>
-      <Container>
-        <Navbar.Brand as={Link} to="/" className={styles.brand}>
+    <Navbar expand="lg" sticky="top" data-bs-theme="dark" className={styles.customNavbar}>
+      <Container fluid>
+        <Navbar.Brand href="#home" className="me-auto">
           Thomas
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            <Nav.Link as={Link} to="/" className={styles.navLink}>
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about" className={styles.navLink}>
-              About
-            </Nav.Link>
-            <Nav.Link as={Link} to="/projects" className={styles.navLink}>
-              Projects
-            </Nav.Link>
-            <Nav.Link as={Link} to="/contact" className={styles.navLink}>
-              Contact
-            </Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home1">Home</Nav.Link>
+            <Nav.Link href="#link2">Projects</Nav.Link>
+            <Nav.Link href="#link3">About</Nav.Link>
+            <Nav.Link href="#link4">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
