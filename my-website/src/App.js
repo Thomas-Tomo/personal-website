6f7d1b8a755c1cc391f564dd026dc514347fb6e8
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-06-03 17:36:35 +0100

    Update App.js with Footer component

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -3,12 +3,14 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import styles from "./App.module.css";
 import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Container fluid className={styles.appContainer}>
       <Header />
       <MainContent />
+      <Footer />
     </Container>
   );
 }
