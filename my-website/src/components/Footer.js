9dc1f923ec63378b22108eb1dbef5abfe984d595
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-06-03 19:05:38 +0100

    Refactor footer to use react bootstrap

--- my-website/src/components/Footer.js
+++ my-website/src/components/Footer.js
@@ -1,27 +1,31 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import styles from "../assets/styles/Footer.module.css";
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <Container>
-        <div className={styles.links}>
-          <a
-            href="https://www.linkedin.com/in/thomasdomitrovic/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a
-            href="https://github.com/Thomas-Tomo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github"></i>
-          </a>
-        </div>
+        <Row className={styles.row}>
+          <Col>
+            <a
+              href="https://www.linkedin.com/in/thomasdomitrovic/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </Col>
+          <Col>
+            <a
+              href="https://github.com/Thomas-Tomo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
