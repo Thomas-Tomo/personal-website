15e4a8956f6f463da52514f9bd4c111f3c6dd017
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-06-04 19:25:11 +0100

    Refactor Projects.js to separate concerns from api fetches

--- my-website/src/components/Projects.js
+++ my-website/src/components/Projects.js
@@ -1,125 +1,49 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Card, Row, Col, Button } from "react-bootstrap";
 import styles from "../assets/styles/Projects.module.css";
+import {
+  fetchRepos,
+  fetchStarCount,
+  fetchCommitCount,
+  fetchLanguages,
+} from "../utils/githubApi";
 
 const Projects = () => {
   const [selectedRepos, setSelectedRepos] = useState([]);
 
   useEffect(() => {
-    const fetchRepos = async () => {
-      try {
-        const response = await axios.get("https://api.github.com/user/repos", {
-          headers: {
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-          },
-          params: {
-            per_page: 100,
-          },
-        });
+    const fetchData = async () => {
+      const repos = await fetchRepos();
+      const filteredRepos = repos.filter((repo) =>
+        [
+          "devexchange",
+          "the_fork_awakens",
+          "Lunar-Escape",
+          "charades",
+          "woodland-whispers-retreat",
+          "hangman",
+          "capital-cities",
+        ].includes(repo.name)
+      );
 
-        // Filter out only the repositories I want to display
-        const filteredRepos = response.data.filter((repo) =>
-          [
-            "devexchange",
-            "the_fork_awakens",
-            "Lunar-Escape",
-            "charades",
-            "woodland-whispers-retreat",
-            "hangman",
-            "capital-cities",
-          ].includes(repo.name)
-        );
+      const reposWithDetails = await Promise.all(
+        filteredRepos.map(async (repo) => {
+          const starCount = await fetchStarCount(repo.owner.login, repo.name);
+          const commitCount = await fetchCommitCount(
+            repo.owner.login,
+            repo.name
+          );
+          const languages = await fetchLanguages(repo.owner.login, repo.name);
+          return { ...repo, starCount, commitCount, languages };
+        })
+      );
 
-        // Fetch details for each repository (star count, commit count, languages, and homepage)
-        const reposWithDetails = await Promise.all(
-          filteredRepos.map(async (repo) => {
-            const starCount = await fetchStarCount(repo.owner.login, repo.name);
-            const commitCount = await fetchCommitCount(
-              repo.owner.login,
-              repo.name
-            );
-            const languages = await fetchLanguages(repo.owner.login, repo.name);
-            return { ...repo, starCount, commitCount, languages };
-          })
-        );
-
-        // Sort repositories by commit count in descending order
-        reposWithDetails.sort((a, b) => b.commitCount - a.commitCount);
-
-        setSelectedRepos(reposWithDetails);
-      } catch (error) {
-        console.error("Error fetching repositories: ", error);
-      }
+      reposWithDetails.sort((a, b) => b.commitCount - a.commitCount);
+      setSelectedRepos(reposWithDetails);
     };
 
-    fetchRepos();
+    fetchData();
   }, []);
-
-  const fetchStarCount = async (owner, repoName) => {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/repos/${owner}/${repoName}`,
-        {
-          headers: {
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-          },
-        }
-      );
-      return response.data.stargazers_count;
-    } catch (error) {
-      console.error("Error fetching star count: ", error);
-      return 0;
-    }
-  };
-
-  const fetchCommitCount = async (owner, repoName) => {
-    try {
-      let page = 1;
-      let totalCount = 0;
-      while (true) {
-        const response = await axios.get(
-          `https://api.github.com/repos/${owner}/${repoName}/commits`,
-          {
-            headers: {
-              Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-            },
-            params: {
-              per_page: 100,
-              page: page,
-            },
-          }
-        );
-        const commits = response.data;
-        totalCount += commits.length;
-        if (commits.length < 100) {
-          break; // No more pages available
-        }
-        page++;
-      }
-      return totalCount;
-    } catch (error) {
-      console.error("Error fetching commit count: ", error);
-      return 0;
-    }
-  };
-
-  const fetchLanguages = async (owner, repoName) => {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/repos/${owner}/${repoName}/languages`,
-        {
-          headers: {
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-          },
-        }
-      );
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching languages: ", error);
-      return {};
-    }
-  };
 
   return (
     <div className={styles.container}>
