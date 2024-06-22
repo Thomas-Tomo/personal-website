2ae1df0e6f4325179921dc2f7e446bb476dd1132
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-06-22 13:08:01 +0100

    Update contact page design

--- my-website/src/components/Contact.js
+++ my-website/src/components/Contact.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
-import Button from 'react-bootstrap/Button';
-import styles from '../assets/styles/Contact.module.css';
-
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import Button from "react-bootstrap/Button";
+import styles from "../assets/styles/Contact.module.css";
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    email: "",
+    message: "",
   });
 
   const handleChange = (e) => {
@@ -19,69 +18,83 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-      .then((result) => {
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
 
     setFormData({
-      name: '',
-      email: '',
-      message: ''
+      name: "",
+      email: "",
+      message: "",
     });
   };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>Contact Me</h1>
-      <div className={styles.card}>
-        <form className={styles.cardBody} onSubmit={sendEmail}>
-          <div className={styles.cardBody}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className={styles.cardBody}>
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className={styles.cardBody}>
-            <label>Message</label>
-            <textarea
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-          </div>
-          <div className={styles.buttonsContainer}>
-          <Button type="submit" variant="outline-light" className="mt-3">
-              Send Email
-            </Button>
-            <Button
-              variant="outline-light"
-              className="mt-3"
-              onClick={() =>
-                window.open('https://wa.me/3530879735102', '_blank')
-              }
-            >
-              Send WhatsApp Message
-            </Button>
-          </div>
-        </form>
+
+      <div className={styles.whatsappContainer}>
+        <h2 className={styles.h2}>Let's Connect on WhatsApp</h2>
+        <p className={styles.whatsappMessage}>
+          Send me a message on WhatsApp to get in touch. I'm ready to
+          collaborate!
+        </p>
+        <Button
+          variant="outline-success"
+          className={styles.whatsappButton}
+          onClick={() => window.open("https://wa.me/3530879735102", "_blank")}
+        >
+          Send WhatsApp Message
+        </Button>
+      </div>
+
+      <div className={styles.emailContainer}>
+        <h2 className={styles.h2}>Send me an Email</h2>
+        <div className={styles.card}>
+          <form className={styles.cardBody} onSubmit={sendEmail}>
+            <div className={styles.cardBody}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className={styles.cardBody}>
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className={styles.cardBody}>
+              <label>Message</label>
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+            <div className={styles.buttonsContainer}>
+              <Button type="submit" variant="outline-light" className="mt-3">
+                Send Email
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
