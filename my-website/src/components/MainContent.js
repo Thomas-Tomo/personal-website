ed6980a2933fee72ffb716a6ee6c2c9eb9d18823
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2024-06-22 13:35:06 +0100

    Fix button size on main page

--- my-website/src/components/MainContent.js
+++ my-website/src/components/MainContent.js
@@ -10,13 +10,13 @@ function MainContent() {
       <Row className={styles.mainContent}>
         <Col
           md={6}
-          className={`order-md-2 d-flex align-items-center justify-content-center ${styles.image}`} // Image column
+          className={`order-md-2 d-flex align-items-center justify-content-center ${styles.image}`}
         >
           <img src={image} alt="Example" className="img-fluid" />
         </Col>
         <Col
           md={6}
-          className={`order-md-1 d-flex flex-column justify-content-center align-items-center ${styles.text}`} // Text column
+          className={`order-md-1 d-flex flex-column justify-content-center align-items-center ${styles.text}`}
         >
           <p className="text-center">
             I'm Thomas-Tomo Domitrovic, Full Stack Software Developer
@@ -25,11 +25,20 @@ function MainContent() {
             Unlocking the power of technology to drive your business forward is
             my mission.
           </h2>
-          <Link to="/contact">
-            <Button variant="outline-light" className={styles.contact}>
+          <Link to="/contact" className={styles.buttonLink}>
+            <Button variant="outline-light" className={`${styles.contact} ${styles.button}`}>
               Contact Me
             </Button>
           </Link>
+          <Button
+            variant="outline-success"
+            className={`${styles.contact} ${styles.button}`}
+            onClick={() =>
+              window.open("https://wa.me/3530879735102", "_blank")
+            }
+          >
+            Send WhatsApp Message
+          </Button>
         </Col>
       </Row>
     </Container>
